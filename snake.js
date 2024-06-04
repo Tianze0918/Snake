dbb448c0117bbb85ccba106b0c2b865877a6f7ac
Diya Baliga <diyabaliga@Diyas-MacBook-Air.local>
2024-06-04 00:57:42 -0700

    basic worm collisions and growth, and candy edits - worm movement is not complete

--- snake.js
+++ snake.js
@@ -22,51 +22,6 @@ class Cube extends Shape {
     }
 }
 
-class Cube_Outline extends Shape {
-    constructor() {
-        super("position", "color");
-        const white=hex_color('#ffffff');
-        // When a set of lines is used in graphics, you should think of the list entries as
-        // broken down into pairs; each pair of vertices will be drawn as a line segment.
-        // Note: since the outline is rendered with Basic_shader, you need to redefine the position and color of each vertex
-
-        this.arrays.position = Vector3.cast(
-        [-1, -1, -1], [1, -1, -1],
-        [-1, -1, 1], [1, -1, 1],  
-        [1, 1, -1], [-1, 1, -1],   
-        [1, 1, 1], [-1, 1, 1],     
-    
-
-        [-1, -1, -1], [-1, -1, 1], 
-        [-1, 1, -1], [-1, 1, 1],   
-        [1, -1, 1], [1, -1, -1],   
-        [1, 1, 1], [1, 1, -1],    
-
-        [-1, -1, 1], [-1, 1, 1],   
-        [1, -1, 1], [1, 1, 1],    
-        [-1, 1, -1], [-1, -1, -1], 
-        [1, 1, -1], [1, -1, -1]    
-        )
-        for (let i=0; i<24; i++){
-            this.arrays.color.push(white);
-        }
-        this.indices=false;
-    }
-}
-
-class Cube_Single_Strip extends Shape {
-    constructor() {
-        super("position", "normal");
-        this.arrays.position = Vector3.cast(
-            [-1, 1, 1], [1, 1, 1], [-1, -1, 1], [1, -1, 1], [-1, 1, -1], [1, 1, -1], [-1, -1, -1], [1, -1, -1])
-        this.arrays.normal = Vector3.cast(
-            [-1, 1, 1], [1, 1, 1], [-1, -1, 1], [1, -1, 1], [-1, 1, -1], [1, 1, -1], [-1, -1, -1], [1, -1, -1]);
-        // Arrange the vertices into a square shape in texture space too:
-        this.indices.push(0,1,2,3,6,7,4,5,0,1,5,3,7,2,6,0,5);
-    
-    }
-}
-
 
 class Base_Scene extends Scene {
     /**
@@ -79,8 +34,6 @@ class Base_Scene extends Scene {
         // At the beginning of our program, load one of each of these shape definitions onto the GPU.
         this.shapes = {
             'cube': new Cube(),
-            'outline': new Cube_Outline(),
-            'single_strip': new Cube_Single_Strip(),
         };
         this.color=[];
         // *** Materials
@@ -95,16 +48,13 @@ class Base_Scene extends Scene {
 
     display(context, program_state) {
         // display():  Called once per frame of animation. Here, the base class's display only does
-        // some initial setup.
-
         // Setup -- This part sets up the scene's overall camera matrix, projection matrix, and lights:
         if (!context.scratchpad.controls) {
             this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
             // Define the global camera and projection matrices, which are stored in program_state.
             program_state.set_camera(Mat4.translation(-7, -7, -30));
         }
-        program_state.projection_transform = Mat4.perspective(
-            Math.PI / 4, context.width / context.height, 1, 100);
+        program_state.projection_transform = Mat4.perspective(Math.PI / 4, context.width / context.height, 1, 100);
 
         // *** Lights: *** Values of vector or point lights.
         const light_position = vec4(0, 5, 5, 1);
@@ -117,58 +67,58 @@ export class snake extends Base_Scene {
     constructor(){
         super();
         this.shapes = {
-            apple1: new defs.Subdivision_Sphere(4),
-            apple2: new defs.Subdivision_Sphere(4),
-            apple3: new defs.Subdivision_Sphere(4),
-            apple4: new defs.Subdivision_Sphere(4),
-            apple5: new defs.Subdivision_Sphere(4),
+            apple: new defs.Subdivision_Sphere(4),
             worm: new defs.Subdivision_Sphere(4),
             cube: new defs.Cube,
         }
 
         this.materials = {
             apple: new Material(new defs.Phong_Shader(),
-            {ambient: 1, diffusivity: .6, color: hex_color("#ffffff")}),
+                {ambient: 1, diffusivity: .6, color: hex_color("#ffffff")}),
             plastic: new Material(new defs.Phong_Shader(),
                 { ambient: .5, diffusivity: .6, specularity: 0.3, color: hex_color("#ffffff") }),
             worm: new Material(new defs.Phong_Shader(),
                 { ambient: .5, diffusivity: .6, specularity: 0.3, color: hex_color("#8B4513") }),
+            worm_hit: new Material(new defs.Phong_Shader(),
+                { ambient: .5, diffusivity: .6, specularity: 0.3, color: hex_color("#00FFFF") }),
         }
 
-        this.worm_position = vec3(0, 0, 1);
-        this.direction = vec3(0, 0, 0);
-        this.apples=[0,0,0,0,0,0,0,0,0,0];
+        // board variables
+        this.board_width = 10;
+        this.board_height = 10;
+
+        // worm variables
+        this.worm_position = Mat4.identity().times(Mat4.translation(0,0,1.8));
+        this.worm_body = [];
+        this.worm_direction = vec3(0, 0, 0);
+        this.worm_speed = 0.05;
+
+        // candy variables
+        this.candies = []
         this.counter=0;
-        this.matrices = [
-            Mat4.identity(), // Matrix for apple1
-            Mat4.identity(), // Matrix for apple2
-            Mat4.identity(), // Matrix for apple3
-            Mat4.identity(), // Matrix for apple4
-            Mat4.identity()  // Matrix for apple5
-        ];
-        this.index = [0,0,0,0,0];
-        this.lastAppleTime = 0;
+        this.last_candy_time = 0;
+        this.candy_size = 0.8;
+
+        // game state variables
+        this.collision_detected = false;
+        this.game_over_flag = false;
     }
 
 
     make_control_panel() {
         // Draw the scene's buttons, setup their actions and keyboard shortcuts, and monitor live measurements.
-        this.key_triggered_button("Change Direction", ["c"], this.set_colors);
-        this.key_triggered_button("Up", ["i"], () => this.direction = vec3(0, 0.1, 0));
-        this.key_triggered_button("Down", ["k"], () => this.direction = vec3(0, -0.1, 0));
-        this.key_triggered_button("Left", ["j"], () => this.direction = vec3(-0.1, 0, 0));
-        this.key_triggered_button("Right", ["l"], () => this.direction = vec3(0.1, 0, 0));
+        this.key_triggered_button("Up", ["i"], () => this.worm_direction = vec3(0, this.worm_speed, 0));
+        this.key_triggered_button("Down", ["k"], () => this.worm_direction = vec3(0, -this.worm_speed, 0));
+        this.key_triggered_button("Left", ["j"], () => this.worm_direction = vec3(-this.worm_speed, 0, 0));
+        this.key_triggered_button("Right", ["l"], () => this.worm_direction = vec3(this.worm_speed, 0, 0));
     }
 
     drawboard(context, program_state){
-
         const green=hex_color('#568b34');
         const green2=hex_color('#49752d');
 
-        
-
-        for (let i = 0; i < 8; i++) {
-            for (let j = 0; j < 8; j++) {
+        for (let i = 0; i < this.board_width; i++) {
+            for (let j = 0; j < this.board_height; j++) {
                 let model_transform = Mat4.identity().times(Mat4.translation(i * 2, j * 2, 0));
                 let diff=i+j;
                 if (diff%2==0){
@@ -180,37 +130,41 @@ export class snake extends Base_Scene {
         }
     }
 
-    drawapple(context, program_state, matrix, t){
-        let row=0;
-        let column=0;
+    generate_candy() {
+        // generate coordinates
+        let row = Math.floor(this.board_height*Math.random());
+        let column = Math.floor(this.board_width*Math.random());
 
-        
-        while(this.find(this.apples, 0, row) && this.find(this.apples, 1, column)){
-            row=Math.floor(8*Math.random());
-            column=Math.floor(8*Math.random());
-        }
-        this.apples[this.counter*2]=row;
-        this.apples[this.counter*2+1]=column;
-        matrix = matrix.times(Mat4.translation(row * 2, column * 2, 2)).times(Mat4.scale(0.8,0.8,0.8));
+        // make matrix
+        let matrix = Mat4.identity().times(Mat4.translation(row * 2, column * 2, 1.8)).times(Mat4.scale(this.candy_size,this.candy_size,this.candy_size));
 
-        let z = Math.floor(t);
-
-
-    
-        // console.log("Apple drawn")
-        console.log(row)
-        console.log(column)
-
-        return matrix;
+        // push onto candies array
+        this.candies.push(matrix);
     }
 
-    find(array, rc, value){
-        for (let i=rc; i<array.length; i=i+2){
-            if(array[i]==value){
-                return true;
+    detect_candy_collision() {
+        this.collision_detected = false;
+        for(let i = 0; i < this.counter; i ++) {
+            let diff = this.worm_position.map((item, index)=> item[3] - this.candies[i][index][3]);
+            let dist = Math.sqrt(diff.reduce((accumulator, currentValue) => accumulator + currentValue**2, 0));
+            if(dist < (1 + this.candy_size)) { // should adjust if we change the snake side!
+                this.collision_detected = true;
+                this.worm_body.push(this.worm_position.times(Mat4.translation(-1.5* this.worm_direction[0] / this.worm_speed, -1.5 * this.worm_direction[1]/ this.worm_speed, 0)))
+                this.candies.pop(i);
+                this.counter -= 1;
             }
         }
-        return false;
+    }
+
+    detect_end_collision() {
+        // check if worm off board
+        let x = this.worm_position[0][3];
+        let y = this.worm_position [1][3];
+        if (x < -0.25 || x >= (this.board_width-1) * 2 || y < -0.25 || y >= (this.board_height -1) * 2) {
+            this.game_over_flag = true;
+        }
+        
+        // add check if worm hits itself - should be similar to candy collision 
     }
 
 
@@ -221,40 +175,44 @@ export class snake extends Base_Scene {
 
         const t = program_state.animation_time / 1000
 
-        this.worm_position = this.worm_position.plus(this.direction.times(0.1));
-
-        // Draw worm
-        let worm_transform = Mat4.identity().times(Mat4.translation(this.worm_position[0] * 2, this.worm_position[1] * 2, this.worm_position[2]));
-        this.shapes.worm.draw(context, program_state, worm_transform, this.materials.worm);
-
-
-
-
-        if( this.counter<5 && (t - this.lastAppleTime > 5)){
-            let model_transform = this.drawapple(context, program_state, this.matrices[this.counter], t);
-            this.matrices[this.counter] = model_transform;
-            this.index[this.counter]=1;
-            this.counter++;
-            this.lastAppleTime = t;
-            console.log("Apple 1 drawn");
-       }
-
-
-        if (this.index[0]!=0){
-            this.shapes.apple1.draw(context, program_state, this.matrices[0], this.materials.plastic.override({color: color}));
-            // console.log("Apple 1 drawn");
-        }if(this.index[1]!=0){
-            this.shapes.apple2.draw(context, program_state, this.matrices[1], this.materials.plastic.override({color: color}));
-        }if(this.index[2]!=0){
-            this.shapes.apple3.draw(context, program_state, this.matrices[2], this.materials.plastic.override({color: color}));
-        }if(this.index[3]!=0){
-            this.shapes.apple4.draw(context, program_state, this.matrices[3], this.materials.plastic.override({color: color}));
-        }if(this.index[4]!=0){
-            this.shapes.apple5.draw(context, program_state, this.matrices[4], this.materials.plastic.override({color: color}));
+        // check if worm is in game ending stat (hit wall/self)
+        this.detect_end_collision();
+        if(this.game_over_flag) {
+            this.worm_direction = vec3(0, 0, 0);
         }
 
+        // check if worm has hit candy and adjust state as required
+        this.detect_candy_collision();
+
+        // move worm
+
+        for(let i = this.worm_body.length - 1; i > 0; i --) {
+            this.worm_body[i] = this.worm_body[i-1].times(Mat4.translation(-1.5 * this.worm_direction[0] / this.worm_speed, -1.5 * this.worm_direction[1]/ this.worm_speed, 0));
+        }
+        if(this.worm_body.length > 0) {
+            this.worm_body[0] = this.worm_position.times(Mat4.translation(-1.5 * this.worm_direction[0] / this.worm_speed, -1.5 * this.worm_direction[1]/ this.worm_speed, 0));
+        }
+        this.worm_position = this.worm_position.times(Mat4.translation(this.worm_direction[0], this.worm_direction[1], 0));
 
 
+        // draw worm
+        this.shapes.worm.draw(context, program_state, this.worm_position, this.collision_detected ? this.materials.worm_hit : this.materials.worm);
+        this.worm_body.map((elem) => 
+            this.shapes.worm.draw(context, program_state, elem, this.collision_detected ? this.materials.worm_hit : this.materials.worm));
+
+
+
+        // generate new candies as needed
+        if( this.counter == 0 || this.counter < 5 && (t - this.last_candy_time > 5)){
+            this.generate_candy();
+            this.counter++;
+            this.last_candy_time = t;
+            console.log("candy drawn");
+       }
+
+       // draw candies
+       this.candies.map(((elem)=>
+            this.shapes.apple.draw(context, program_state, elem, this.materials.plastic.override({color: color}))
+        ));
     }
-
 }
